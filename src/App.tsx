ea976e0232cc0cb6d6b4b682b251f12fa8daf1dd
Jean Meira <jean.meira10@hotmail.com>
2023-06-10 15:06:41 -0300

    feat: configure chat engine

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
 function App() {
-  return <h1>hello world</h1>;
+  return (
+    <div className="App">
+      <ChatEngine
+        height="100vh"
+        projectID="079c45f9-cd86-4446-98fc-880f72ec5b21"
+        userName="jeanmeira"
+        userSecret="47151785"
+      />
+    </div>
+  );
 }
 
 export default App;
